9f0d59d57563127f2e45b8afd5baa7ab19064431
Ash Furrow <ash@floatcard.com>
2024-12-02 21:59:06 -0400

    Adds day two part two solution.

--- day_two.rb
+++ day_two.rb
@@ -5,7 +5,7 @@ def is_safe?(report)
 
   increasing = report[0] < report[1]
   acc = report[0]
-  for level in report[1...]
+  report[1...].each do |level|
     if increasing
       return false if level <= acc
     else # decreasing
@@ -20,11 +20,36 @@ def is_safe?(report)
   return true
 end
 
+def remove_index(arr, index)
+  arr.dup.tap{|i| i.delete_at(index)}
+end
+
+
+def is_safe_dampener?(report)
+  # Any report of length 1 is valid, and we can remove any single element. 
+  return true if report.length <= 2 
+  
+  # Let's handle the easy case, if the report is conventionally safe.
+  return true if is_safe?(report)
+
+  # Okay, so the report is not "safe." 
+  # The naive approach is to remove elements from the array and check if 
+  # removing just one makes it safe. Inelegant.
+  # The more sophisticated approach is to rewrite is_safe? in a way that detects
+  # individual problems based on context.
+  # But the dampener was installed after-the-fact, not realtime. And the input
+  # data is small enough that the naive approach works.
+  
+  report.each_with_index do |_, index|
+    return true if is_safe?(remove_index(report, index))
+  end
+
+  return false
+end
+
 reports = File.open('./day_two.input')
   .readlines # read the lines
   .map { |line| line.split(' ').map(&:to_i) } # split the levels and convert to ints
 
-safe_reports = 0
-reports.each { |r| safe_reports += is_safe?(r) ? 1 : 0}
-
-puts safe_reports
+puts reports.select{ |r| is_safe?(r) }.length
+puts reports.select{ |r| is_safe_dampener?(r) }.length

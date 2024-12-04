93105135c40afbaa54e03f61a8c55012d2fde50e
Ash Furrow <ash@floatcard.com>
2024-12-03 22:58:27 -0400

    Adds in-progress solution.

--- day_three.rb
+++ day_three.rb
@@ -1,8 +1,16 @@
 #!/usr/bin/env ruby
 
-puts File.open('./day_three.input')
-  .read # read the file
-  .scan(/mul\((\d{1,3}),(\d{1,3})\)/)
+
+def total(input)
+  input.scan(/mul\((\d{1,3}),(\d{1,3})\)/)
   .map { |r| r[0].to_i * r[1].to_i }
   .sum
+end
+  
+input = File.open('./day_three.input').read.gsub("\n", '') # read the file
+puts total(input)
 
+# TODO: this doesn't work yet
+matches = input.scan(/(.*?)don't\(\).*?do\(\)/)
+puts matches.map { |result| total(result[0]) }.sum +
+  total(matches[-1][0].split("do()")[1...].join(""))
